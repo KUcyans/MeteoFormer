b7a19d77b775673a7ee3d8f5eb14cf806425b35d
KUcyans <cyan.jo@proton.me>
2026-05-27 22:38:07 +0200

    no clear advantage of using 64 or 128 for window size

--- RunTesting.sh
+++ RunTesting.sh
@@ -22,7 +22,7 @@ sleep 1
 # MANUAL SELECTION OF TRAINING RUN
 # -------------------------------
 DATE=20260527   # training run date: YYYYMMDD
-TIME=210527     # training run time: HHMMSS
+TIME=215154     # training run time: HHMMSS
 # -------------------------------
 
 # Timestamp for THIS test run
